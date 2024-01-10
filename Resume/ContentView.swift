ab58b0969c2da48c13b4b874b411cfb3ec3e0771
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2024-01-10 11:33:52 -0600

    Initial Commit

---
+++ Resume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Resume
+//
+//  Created by Jorge on 10/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Resume/ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeApp.swift
+//  Resume
+//
+//  Created by Jorge on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
